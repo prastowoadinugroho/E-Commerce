6cb6bf243b2b244c12f538e417b3193092a7076a
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-02-22 08:09:47 +0700

    Create product

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(cors());
 
 app.use('/api/user/', require('./routes/auth_route'));
 app.use('/api/category/', require('./routes/category_route'));
+app.use('/api/product/', require('./routes/product_route'));
 
 app.get("/",(req,res) => {
     res.send('tes => home page');
